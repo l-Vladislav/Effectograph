8464526413d5a2d3b404a87c7aa370e276e5a5c2
Vladislav <warp.adm@gmail.com>
2024-07-10 14:41:43 +0300

    Base project configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { ImageUploadComponent } from "./components/image-upload/image-upload.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet, ImageUploadComponent],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css"
 })
 export class AppComponent {
-  title = 'effectograph';
+	title = "effectograph";
 }
